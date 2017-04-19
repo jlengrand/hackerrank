4142c9f7b79dff28018e0a0ba08446268df8bfbc
Julien Lengrand-Lambert <jlengrand@gmail.com>
2017-04-19 22:49:36 +0200

    Solves PlusMinus

---
+++ src/main/scala/algorithms/plusMinus/solution.scala
@@ -0,0 +1,11 @@
+object Solution {
+
+  def main(args: Array[String]) {
+    var n = scala.io.StdIn.readInt()
+    val arr = scala.io.StdIn.readLine().split(" ").map(_.toInt).toList;
+
+    println(arr.filter(v => v > 0).length.toFloat/n)
+    println(arr.filter(v => v < 0).length.toFloat/n)
+    println(arr.filter(v => v == 0).length.toFloat/n)
+  }
+}
